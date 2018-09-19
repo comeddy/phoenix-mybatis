1e2984db232f326df583b97a424b43d5dfb84616
comeddy <comeddy@gmail.com>
2018-09-19 17:31:38 +0900

    phoenix version change

--- src/main/java/com/lgd/hadoop/fdc/FdcMybatisTest.java
+++ src/main/java/com/lgd/hadoop/fdc/FdcMybatisTest.java
@@ -4,36 +4,38 @@ import java.util.Properties;
 
 import com.lgd.hadoop.fdc.services.FdcService;
 import com.lgd.hadoop.fdc.domain.FdcTest;
-import org.apache.log4j.*;
+//import org.apache.log4j.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FdcMybatisTest {
-    private static final Logger LOG = LoggerFactory.getLogger(FdcMybatisTest.class);
+    //private static final Logger LOG = LoggerFactory.getLogger(FdcMybatisTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(FdcMybatisTest.class);
     private static final Properties properties = System.getProperties();
 
     private static FdcService fdcService;
 
     public static void main( String[] args )
     {
-        System.out.println("====LGD Phoenix myBatis DevCode====");
+  
+        logger.info("====LGD Phoenix myBatis DevCode====");
         fdcService = new FdcService();
 
-        System.out.println("== select all fdc test ==");
+        logger.info("== select all fdc test ==");
         List<FdcTest> fdcTestList = fdcService.findAllFdcTest();
         for (FdcTest fdcTest : fdcTestList){
-            System.out.println("\tRow: " + fdcTest.getMyKey() + " = " + fdcTest.getMyColumn());
+            logger.info("\tRow: " + fdcTest.getMyKey() + " = " + fdcTest.getMyColumn());
         }
 
-        System.out.println("== select fdc test by id 2==");
+        logger.info("== select fdc test by id 2==");
         FdcTest fdcTest2 = fdcService.findFdcTestById(2);
-        System.out.println("\tRow: " + fdcTest2.getMyKey() + " = " + fdcTest2.getMyColumn());
+        logger.info("\tRow: " + fdcTest2.getMyKey() + " = " + fdcTest2.getMyColumn());
 
         FdcTest fdcTest3 = new FdcTest(5,"5th fdc test record");
-        System.out.println("== insert fdc test by id 5==");
+        logger.info("== insert fdc test by id 5==");
         fdcService.createFdcMapper(fdcTest3);
 
-        System.out.println("== select all fdc test again ==");
+        logger.info("== select all fdc test again ==");
         List<FdcTest> fdcTestList2 = fdcService.findAllFdcTest();
         for (FdcTest fdcTest : fdcTestList2){
             System.out.println("\tRow: " + fdcTest.getMyKey() + " = " + fdcTest.getMyColumn());
